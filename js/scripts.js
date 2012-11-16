d1550b72c608ff104a4889578516534082cacb0f
Paul Foster <user@server>
2012-11-16 22:57:40 +1100

    Add tootips with info buttons

--- js/scripts.js
+++ js/scripts.js
@@ -25,21 +25,21 @@ $(document).ready(function() {
       $("#inner_hsplitter").wijsplitter("refresh");
       
       // Resize textarea
-      $("#markup_textarea").css('height', $("#markup_split").height() - 40);
+      $("#markup_textarea").css('height', $("#markup_split").height() - 50);
     },
     collapsed : function(e) {
       $("#vsplitter").wijsplitter("refresh");
       $("#inner_hsplitter").wijsplitter("refresh");
       
       // Resize textarea
-      $("#markup_textarea").css('height', $("#markup_split").height() - 40);
+      $("#markup_textarea").css('height', $("#markup_split").height() - 50);
     },
     sized : function(e) {
       $("#vsplitter").wijsplitter("refresh");
       $("#inner_hsplitter").wijsplitter("refresh");
       
       // Resize textarea
-      $("#markup_textarea").css('height', $("#markup_split").height() - 40);
+      $("#markup_textarea").css('height', $("#markup_split").height() - 50);
     }
   });
   $("#vsplitter").wijsplitter({
@@ -87,20 +87,33 @@ $(document).ready(function() {
       $("#vsplitter").wijsplitter("refresh");
       
       // Resize textarea
-      $("#yahoo_input_textarea").css('height', $("#yahoo_input_split").height() - 40);
+      $("#yahoo_input_textarea").css('height', $("#yahoo_input_split").height() - 50);
     }
   });
   
   // Set initial textarea dimensions
   $("#markup_textarea").css('width', $("#markup_split").width() - 30);
-  $("#markup_textarea").css('height', $("#markup_split").height() - 40);
+  $("#markup_textarea").css('height', $("#markup_split").height() - 50);
   
   $("#yahoo_input_textarea").css('width', $("#yahoo_input_split").width() - 30);
-  $("#yahoo_input_textarea").css('height', $("#yahoo_input_split").height() - 40);
+  $("#yahoo_input_textarea").css('height', $("#yahoo_input_split").height() - 50);
   
   // The following fixes bug when resizing will leave a strip on the right until it is resized again
   $("#outer_hsplitter").wijsplitter("refresh");
   $("#vsplitter").wijsplitter("refresh");
   
   // TODO: Change color of label when in focus
+  
+
+	// Handle info popovers
+	(function() {
+		// FIXME: Make popovers hide when you click away
+		// $("[rel=popover_bottom]").tooltip();
+		// $("[rel=popover_bottom]").popover({placement:'bottom'});
+
+		// $("[rel=popover]").popover();
+		$(".btn-info").popover();
+
+	})(); 
+
 });
